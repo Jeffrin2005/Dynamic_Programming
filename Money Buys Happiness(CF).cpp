1dad8b548449f8ca8ab3ac9b55efca01152a0d19
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-05-21 20:25:14 +0530

    Update Money Buys Happiness(CF).cpp

--- Money Buys Happiness(CF).cpp
+++ Money Buys Happiness(CF).cpp
@@ -1,4 +1,23 @@
-
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std; 
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+typedef vector<int> vi;
+#define cl clear
+#define stt string 
+#define ll  long long 
+#define ed end
+#define vec vector
+#define sz(a) int((a).size())
+#define pp pop_back
+#define minii min_element
+#define maxii max_element
+#define rv reverse
+#define fr front
+#define ss second
+#define nl '\n' 
 void solve(){
     ll m, x;
     cin >> m >> x;
@@ -9,8 +28,9 @@ void solve(){
         cin>>c[i]; cin>>h[i];
         sum+=h[i];
     }
-    vector<vector<ll>>dp(m+1, vector<ll>(sum+1, -1e12));
+    vector<vector<ll>>dp(m+1, vector<ll>(sum+1, -1e11));
     dp[0][0] = 0;
+    for(int i=1; i<=m; i++) dp[i][0] = 0;
     for(int i = 1; i <= m; i++){
         for(int j = 0; j <= sum; j++){
            dp[i][j] = dp[i-1][j] + x;
