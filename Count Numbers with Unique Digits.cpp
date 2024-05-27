917a11c3adf3fec1e5795d6ee2a2f74664d6a459
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-05-27 16:56:32 +0530

    Create Count Numbers with Unique Digits.cpp

---
+++ Count Numbers with Unique Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        vector<int> dp(9);
+        dp[0] = 1;
+        vector<int> temp = {9, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for(int i = 1; i <= n; i++)
+        {
+            dp[i] = dp[i - 1];
+            int prod = 1;
+            for(int j = 0; j < i; j++)
+                prod = prod * temp[j];
+            dp[i] += prod; 
+        }
+        return dp[n]; 
+    }
+};
