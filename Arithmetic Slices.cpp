96bfebaf4ccd8e63e8f7a6e0b98eeaf60e31da08
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-07 22:23:10 +0530

    Update Arithmetic Slices.cpp

--- Arithmetic Slices.cpp
+++ Arithmetic Slices.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
+        int n1 = ;
         vector<int>dp(n+1, 0);
         int ans = 0;
         for(int i = 2; i < n; i++){
