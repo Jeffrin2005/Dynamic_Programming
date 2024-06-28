93f32c847295be61abe56c0118d5f0dc4fea1879
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-28 12:41:43 +0530

    Update Is Subsequence.cpp

--- Is Subsequence.cpp
+++ Is Subsequence.cpp
@@ -1,21 +1,23 @@
 class Solution {
+private:
+    // s_index = i;
+    // t_index = j; 
+    bool check(string &s, string &t,int i, int j, vector<vector<int>>&dp){
+        if(i == s.size()) return true;
+        if(j == t.size()) return false;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i] == t[j]){
+            dp[i][j] = check(s, t,i + 1, j + 1, dp);
+        }else{
+            dp[i][j] = check(s, t , i, j + 1, dp );
+        }
+        return dp[i][j];
+    }
 public:
     bool isSubsequence(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true; // Correcting this to true
-        for (int i = 1; i <= m; i++) dp[i][0] = false; // s is not empty, t is empty
-        for (int i = 1; i <= n; i++) dp[0][i] = true; // s is empty, t is not empty
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1]; 
-                } else {
-                    dp[i][j] = dp[i][j - 1]; 
-                }
-            }
-        }
-        return dp[m][n];
+        int n= s.size();
+        int n1 = t.size(); 
+        vector<vector<int>>dp(n, vector<int>(n1 , - 1));
+        return check(s, t, 0, 0 , dp);
     }
 };
