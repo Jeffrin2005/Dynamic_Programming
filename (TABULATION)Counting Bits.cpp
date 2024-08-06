1506c42507346bf6ac5d4850d6b462acee903619
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-06 21:49:35 +0530

    Update and rename Counting Bits.cpp to (TABULATION)Counting Bits.cpp

---
+++ (TABULATION)Counting Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+// time comp =o(n) space comp = o(n)
+    vector<int> countBits(int n) {
+      vector<int>dp(n + 1, 0);
+        dp[0] = 0; // Base case: there are no 1s in the binary representation of 0
+        for(int i = 1; i <= n; i++){
+            // The number of 1s in i is the number of 1s in i/2 plus 1 if i is odd
+            dp[i] = (i%2) + dp[i/2];
+        }
+        return dp;  
+        
+    }
+};
