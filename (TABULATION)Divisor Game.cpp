f75d8b208be14c378a22bd7d89e6dd7220a1a8f4
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-09 23:23:12 +0530

    Update and rename Divisor Game.cpp to (TABULATION)Divisor Game.cpp

---
+++ (TABULATION)Divisor Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<int> dp(n + 1, 0); 
+        dp[1] = 0; // Base case: if n = 1, the player loses
+        for (int i = 2; i <= n;i++){
+            for (int x = 1; x < i; x++){
+                if (i % x == 0 && dp[i - x] == 0) { // If the opponent loses from state i - x
+                    dp[i] = 1; // Current player wins
+                    break;
+                }
+            }
+        }
+        return dp[n] == 1; // Return true if dp[n] is 1 (Alice wins)
+    }
+};
