13b87c1027b16fc64cdccabb25351c372185936b
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-09-08 10:12:16 +0530

    Update Climbing Stairs(RECU + MEMO).cpp

--- Climbing Stairs(RECU + MEMO).cpp
+++ Climbing Stairs(RECU + MEMO).cpp
@@ -3,6 +3,7 @@ private:
     int check(vector<int>&dp, int i ){
         if(i <= 1) return 1;
         if(dp[i] != -1) return dp[i];
+        
         dp[i] = check(dp , i - 1) + check(dp , i - 2);
         return dp[i]; 
     }
