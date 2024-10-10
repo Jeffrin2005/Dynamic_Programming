6d84408ed6465281eecb06257a8a791a1aa486be
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-10-10 22:24:16 +0530

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,22 +1,43 @@
+class Solution {// RECUR + MEMO 
+private:
+    bool check(string s, set<string>&set, vector<int>&dp, int start){
+        if(start == s.size()) return true;
+        if(dp[start] != -1) return dp[start];
+        for(int i=start; i<s.size(); i++){
+            if(set.count(s.substr(start,i+1-start)) && check(s,set,dp,i+1)){
+                dp[start] = true;
+                return true; 
+            }
+        }
+        dp[start] = false;
+        return false;
+    
+    }
+public:
 
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int>dp(n+1,-1);
+        set<string>set(wordDict.begin(),wordDict.end());
+        return check(s, set, dp , 0);
+        
+    }
+};// TABULATION
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<bool> dp(n + 1, false);  
-        set<string> dict(wordDict.begin(), wordDict.end());
-
-        dp[0] = true; 
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.count(s.substr(j, i - j))) {
+        set<string> set(wordDict.begin(), wordDict.end()); 
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && set.count(s.substr(j,i-j))){
                     dp[i] = true;
                     break; 
                 }
             }
         }
-
-        return dp[n]; 
+        return dp[n];
     }
 };
