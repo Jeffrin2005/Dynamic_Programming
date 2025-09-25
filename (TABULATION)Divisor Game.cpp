1523a5fe8f26104f25acf5caf39423c93cec214e
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2025-09-25 09:54:21 +0530

    Update (TABULATION)Divisor Game.cpp

--- (TABULATION)Divisor Game.cpp
+++ (TABULATION)Divisor Game.cpp
@@ -5,6 +5,7 @@ public:
         dp[1] = 0; // Base case: if n = 1, the player loses
         for (int i = 2; i <= n;i++){
             for (int x = 1; x < i; x++){
+                
                 if (i % x == 0 && dp[i - x] == 0) { // If the opponent loses from state i - x
                     dp[i] = 1; // Current player wins
                     break;
